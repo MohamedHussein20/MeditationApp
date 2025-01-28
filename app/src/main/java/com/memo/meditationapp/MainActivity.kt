221697af2434a25f0af561e4539175e224fa4780
Mohamed Hussein <mohussein1279@gmail.com>
2025-01-28 21:59:11 +0200

    Finished Features Section

--- app/src/main/java/com/memo/meditationapp/MainActivity.kt
+++ app/src/main/java/com/memo/meditationapp/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.memo.meditationapp.ui.theme.HomeScreen
 
 import com.plcoding.meditationuiyoutube.ui.theme.MeditationAppTheme
 
